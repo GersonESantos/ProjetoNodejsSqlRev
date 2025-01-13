f517861aa49565c88a2475792f3521bbc976bbac
GersonESS <gebhsantos@gmail.com>
2025-01-13 13:58:32 -0300

    a

--- 05 - Framework Express/app.js
+++ 05 - Framework Express/app.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+// Importar módulo express
+const express = require('express');
+
+// App
+const app = express();
+
+// Rota de teste
+app.get('/', function(req, res){
+
+    res.write('Hello World!');
+    res.end();
+});
+
+// Servidor
+app.listen(8080);
