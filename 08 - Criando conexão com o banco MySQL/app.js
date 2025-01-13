f5aabae46681f600b191fba8d9962e03b4409a0c
GersonESS <gebhsantos@gmail.com>
2025-01-13 14:49:01 -0300

    a

--- 08 - Criando conexão com o banco MySQL/app.js
+++ 08 - Criando conexão com o banco MySQL/app.js
@@ -1,13 +1,29 @@
 // Importar módulo express
 const express = require('express');
 
+const mysql = require('mysql2');
+
 // App
 const app = express();
+
+// Conexão com o banco de dados
+const Conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Gabibi89*',
+    database: 'projeto'
+}); 
+// Conectar
+Conexao.connect(function(err){
+    if(err) throw err;
+    console.log('Conectado com sucesso!');
+}
+);
 
 // Rota de teste
 app.get('/', function(req, res){
 
-    res.write('Hello World!');
+    res.write('Utilizando MySQL com Node.js');
     res.end();
 });
 
