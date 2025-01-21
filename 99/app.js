ddfc49eb54bf38bb8ef4839d99dc990f137387e9
GersonESS <gebhsantos@gmail.com>
2025-01-21 14:38:22 -0300

    Update app.js

--- 99/app.js
+++ 99/app.js
@@ -1,50 +1,24 @@
-// Importar módulo express
-const express = require('express');
-
-
-//importar módulo fileupload
-
-const fileUpload = require('express-fileupload');
-// importar módulo express-handlebars
-
-const { engine } = require('express-handlebars');
-// importar módulo de rotas
-const rota_produto = require('./rotas/produtos_rotas');
-
-// App
+// Criar objeto app
 const app = express();
 
-// habilitar fileupload
-app.use(fileUpload());  
+// Rotas
+app.get('/', function(req, res){
+    // Exibir mensagem
+    res.write("Utilizando o Express na KingHost!");
+    
+    // Exibir rota
+    res.end();
+});
 
-// Adicionar bootstrap
+app.get('/:nome', function(req, res){
+    // Exibir mensagem
+    res.write("Oi " + req.params.nome);
+    
+    // Finalizar rota
+    res.end();
+});
 
-app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
-// Adicionar css
+// Porta do servidor app.listen(21100);
 
-app.use('/css', express.static('./css'));
-//Referenciar a pasta imagens
-app.use('/imagens', express.static('./imagens'));
-// Configuração do handlebars
-
-// Configuração do express-handlebars
-app.engine('handlebars', engine({
-    helpers: {
-      // Função auxiliar para verificar igualdade
-      condicionalIgualdade: function (parametro1, parametro2, options) {
-        return parametro1 === parametro2 ? options.fn(this) : options.inverse(this);
-      }
-    }
-  }));
-app.set('view engine', 'handlebars');
-app.set('views', './views');
-// Manipulação de dados via Rotas
-
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
-// rotas
-app.use('/', rota_produto);
-
-// Servidor
+// Porta do servidor
 app.listen(8080);
