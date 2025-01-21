078debae2fa31ce2191ff02399249310a2a8ce37
GersonESS <gebhsantos@gmail.com>
2025-01-21 02:02:47 -0300

    d

---
+++ 52 - NPM na KingHost/app.js
@@ -0,0 +1,22 @@
+// Criar objeto app
+const app = express();
+
+// Rotas
+app.get('/', function(req, res){
+    // Exibir mensagem
+    res.write("Utilizando o Express na KingHost!");
+    
+    // Exibir rota
+    res.end();
+});
+
+app.get('/:nome', function(req, res){
+    // Exibir mensagem
+    res.write("Oi " + req.params.nome);
+    
+    // Finalizar rota
+    res.end();
+});
+
+// Porta do servidor
+app.listen(21100);
