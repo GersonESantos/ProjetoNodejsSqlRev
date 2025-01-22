28d3c6dd87af575958589ed07b8c07c8e45e56ce
GersonESS <gebhsantos@gmail.com>
2025-01-22 17:46:45 -0300

    a

--- 62 - Conexao ver qual/app.js
+++ 62 - Conexao ver qual/app.js
@@ -2,49 +2,19 @@
 const express = require('express');
 
 
-//importar módulo fileupload
-
-//const fileUpload = require('express-fileupload');
-// importar módulo express-handlebars
-
-const { engine } = require('express-handlebars');
-// importar módulo de rotas
-const rota_produto = require('./rotas/produtos_rotas');
 
 // App
 const app = express();
 
-// habilitar fileupload
-app.use(fileUpload());  
+// Conexão com o banco de dados
+const Conexao = require('./bd/conexao_mysql');
 
-// Adicionar bootstrap
+// Rota de teste
+app.get('/', function(req, res){
 
-app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
-// Adicionar css
-
-app.use('/css', express.static('./css'));
-//Referenciar a pasta imagens
-app.use('/imagens', express.static('./imagens'));
-// Configuração do handlebars
-
-// Configuração do express-handlebars
-app.engine('handlebars', engine({
-    helpers: {
-      // Função auxiliar para verificar igualdade
-      condicionalIgualdade: function (parametro1, parametro2, options) {
-        return parametro1 === parametro2 ? options.fn(this) : options.inverse(this);
-      }
-    }
-  }));
-app.set('view engine', 'handlebars');
-app.set('views', './views');
-// Manipulação de dados via Rotas
-
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
-// rotas
-app.use('/', rota_produto);
+    res.write('Utilizando MySQL com Node.js');
+    res.end();
+});
 
 // Servidor
 app.listen(8080);
