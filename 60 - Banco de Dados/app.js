d156b1983d75fabf19836ff35f6ef502ac51a06d
GersonESS <gebhsantos@gmail.com>
2025-01-22 14:57:52 -0300

    a

--- 60 - Banco de Dados/app.js
+++ 60 - Banco de Dados/app.js
@@ -1,13 +1,24 @@
 // Importar módulo express
 const express = require('express');
 
-
+const mysql = require('mysql2');
 
 // App
 const app = express();
 
 // Conexão com o banco de dados
-const Conexao = require('./bd/conexao_mysql');
+const Conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Gabibi89*',
+    database: 'projeto'
+}); 
+// Conectar
+Conexao.connect(function(err){
+    if(err) throw err;
+    console.log('Conectado com sucesso!');
+}
+);
 
 // Rota de teste
 app.get('/', function(req, res){
