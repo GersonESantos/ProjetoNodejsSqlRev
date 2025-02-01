09ec5200fc98107766b79fff3ab848c927e6918d
GersonESS <gebhsantos@gmail.com>
2025-02-01 15:49:07 -0300

    a

--- 05 - Framework Express/app.js
+++ 05 - Framework Express/app.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.write('Hello World!');
+  res.end()
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
